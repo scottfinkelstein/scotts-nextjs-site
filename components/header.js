46b1f9f1f100ddc90fc4678319156d965ab2cfae
Scott Finkelstein <sbf0202@gmail.com>
2022-10-29 16:39:46 -0400

    Cleaned up cards and moved into its own component.

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { BsFillMoonStarsFill, BsFillSunFill, BsCodeSlash } from 'react-icons/bs'
 import logo from '../public/logo.png'
 
@@ -7,9 +8,9 @@ export default function Header(props) {
     return(
         <header className='flex justify-between py-10 px-3 dark:bg-black'>
         <div className="flex items-top gap-1">
-            <a href="/"><Image src= { logo } alt="" width={ 40 } height={ 40 } /></a>
+            <Link href="/"><Image src= { logo } alt="" width={ 40 } height={ 40 } /></Link>
             <div>
-              <h1 className="font-antonio font-medium text-2xl text-sky-800 dark:text-sky-100"><a href="/">Scott Finkelstein</a></h1>
+              <h1 className="font-antonio font-medium text-2xl text-sky-800 dark:text-sky-100"><Link href="/">Scott Finkelstein</Link></h1>
             </div>
         </div>
     
